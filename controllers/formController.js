0289d98b51084ce3ed5249573b954d307e3a40a9
MarcosMCM21 <marcoscorreademelo@gmail.com>
2024-05-10 00:51:36 -0300

    Adicionado a lógica do sendGrid

--- controllers/formController.js
+++ controllers/formController.js
@@ -3,9 +3,29 @@ exports.view = (req, res) => {
 }
 
 exports.post = (req,res) => {
-    console.log(req.query)
-    console.log(req)
-    console.log(res)
+    sendEmail()
     paramEmail = req.body['email']
     res.render('email', { email: paramEmail })
+}
+
+function sendEmail() {
+    // using Twilio SendGrid's v3 Node.js Library
+    // https://github.com/sendgrid/sendgrid-nodejs
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+    to: 'marcoscorreademelo@gmail.com', // Change to your recipient
+    from: 'marcos.mcm@marcoscorreademelo.com.br', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
 }
